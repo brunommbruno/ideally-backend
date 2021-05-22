28a4ae61222f5289459dadb860064c7a1e4b8353
brunommbruno <brunommbruno@gmail.com>
2021-05-22 13:10:41 +0200

    created API controller and completed resource

--- app/Http/Resources/API/IdeaResource.php
+++ app/Http/Resources/API/IdeaResource.php
@@ -14,6 +14,13 @@ class IdeaResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [ 
+            "description" => $this->description,
+            "tags" => $this->tags,
+            "likes" => $this->likes,
+            "bk_color" => $this->bk_color,
+            "txt_color" => $this->txt_color,
+            "user" => $this->user,
+        ];
     }
 }
